f3ba6c45b5ab5f05afaf41b52b9437987745c11c
Amal Araweelo Almis <124199007+amal-araweelo@users.noreply.github.com>
2026-02-22 14:56:33 +0100

    serial fcs check done

--- ex1_fcs_serial_check/rtl/fcs_serial_check.sv
+++ ex1_fcs_serial_check/rtl/fcs_serial_check.sv
@@ -16,13 +16,12 @@ module fcs_serial_check (
 
 
   // Compute feedback bit
-  // Negate data input only if FCS bits or the first 32 bits of payload are being processed
   always_comb begin
-    if (start_cntr < 7'd32 && start_cntr != 7'd0) begin
+    if (start_cntr < 'd32 && start_cntr != 'd0) begin
       fb = ~data_in ^ crc_r[31];  // negate payload bits
     end else if (start_of_frame) begin
       fb = ~data_in ^ crc_r[31]; // negate first bit of payload
-    end else if (fcs_bit_cntr < 7'd32) begin
+    end else if (fcs_bit_cntr < 'd32) begin
       fb = ~data_in ^ crc_r[31]; // negate FCS bits
     end else if (end_of_frame) begin
       fb = ~data_in ^ crc_r[31]; // negate first bit of FCS
@@ -32,27 +31,29 @@ module fcs_serial_check (
   end
 
   // Ouput assignment
-  assign fcs_error = fcs_error_r;
+  assign fcs_error = fcs_error_r; // pulses for one clock cycle at the end of frame if there is an error
 
   always_ff @(posedge clk) begin
+    
+    fcs_error_r <= 'b0; // default to no error, will be set at the end of frame
+
     if (!reset) begin
-      crc_r        <= 32'b0;
-      start_cntr   <= 7'd32;
-      fcs_bit_cntr <= 7'd32;
-      in_frame     <= 1'b0;
-      fcs_error_r  <= 1'b0;
+      crc_r        <= 'b0;
+      start_cntr   <= 'd32;
+      fcs_bit_cntr <= 'd32;
+      in_frame     <= 'b0;
+      fcs_error_r  <= 'b0;
     end else begin
 
       // Start of frame (reset counter and enable in frame)
       if (start_of_frame) begin
-          in_frame <= 1'b1;
-          start_cntr <= start_cntr - 7'd1;
-          
+          in_frame <= 'b1;
+          start_cntr <= 'd31;
       end
 
       // End of frame
       if (end_of_frame) begin
-        fcs_bit_cntr <= fcs_bit_cntr - 7'd1;
+        fcs_bit_cntr <= fcs_bit_cntr - 'd1;
       end
 
       // Generator polynomial: x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
@@ -91,14 +92,23 @@ module fcs_serial_check (
         crc_r[0]  <= fb;
 
         // Decrement start bit counter only when we are in the payload part of the frame
-        if (start_cntr < 7'd32 && start_cntr != 7'd0) start_cntr <= start_cntr - 7'd1;
-        else if (start_cntr == 7'd1) start_cntr <= 7'd0; // keep at 0
+        if (start_cntr < 'd32 && start_cntr != 'd0) start_cntr <= start_cntr - 'd1;
+        else if (start_cntr == 'd1) start_cntr <= 'd0; // keep at 0
 
         // Decrement FCS bit counter only when we are in the FCS part of the frame
-        if (fcs_bit_cntr < 7'd32) fcs_bit_cntr <= fcs_bit_cntr - 7'd1;
-        else if (fcs_bit_cntr == 7'd1) fcs_bit_cntr <= 7'd0;
+        if (fcs_bit_cntr < 'd32) fcs_bit_cntr <= fcs_bit_cntr - 'd1;
+        else if (fcs_bit_cntr == 'd1) fcs_bit_cntr <= 'd0;
 
-        if (fcs_bit_cntr == 7'd0) fcs_error_r <= (crc_r == 32'h0000_0000) ? 'b0: 'b1; // pulses high for one cycle when error is detected at the end of frame
+        // Check for FCS error at the end of frame
+        if (fcs_bit_cntr == 'd0) begin
+          fcs_error_r <= (crc_r == 32'h0000_0000) ? 'b0: 'b1;
+          in_frame <='b0;
+
+          // prepare for next frame
+          crc_r <= '0;
+          start_cntr   <= 'd32;
+          fcs_bit_cntr <= 'd32;
+        end
       end
     end
   end
--- ex1_fcs_serial_check/tb/tb_fcs_serial_check.sv
+++ ex1_fcs_serial_check/tb/tb_fcs_serial_check.sv
@@ -32,7 +32,10 @@ module tb_fcs_serial_check;
   logic [fcs.PAYLOAD_LEN-1:0] payload_neg;
   logic [fcs.PAYLOAD_LEN+31:0] frame;
 
-  task automatic simple_test_fixed_payload();
+
+
+  ///////////////////////////// TEST TASKS //////////////////////////////
+  task automatic test_fixed_payload();
 
     // Fixed payload
     payload = 512'h0010_A47B_EA80_0012_3456_7890_0800_4500_002E_B3FE_0000_8011_0540_C0A8_002C_C0A8_0004_0400_0400_001A_2DE8_0001_0203_0405_0607_0809_0A0B_0C0D_0E0F_1011; // fixed payload for debugging
@@ -40,9 +43,6 @@ module tb_fcs_serial_check;
     crc = 32'hE6C53DB2;
     // crc_neg = ~crc; // for debugging
     frame = {payload, crc}; // frame to be fed to fcs_serial_check
-
-    $display("Payload 32 MSB neg: 0x%h", payload_neg);
-    $display("CRC neg: 0x%h", crc_neg);
 
     // Send serially
     i_fcs_if.cb.start_of_frame <= 'b1; // pulse start of frame
@@ -57,26 +57,20 @@ module tb_fcs_serial_check;
       @(i_fcs_if.cb);
       i_fcs_if.cb.end_of_frame <= 'b0;
     end
+    @(i_fcs_if.cb); // wait one clock cycle for the final crc register update
 
-    // Wait 31 cycles to ensure fcs_serial_check processes final bits
-    repeat(31) begin
-      // $display("fcs_error = b%b", i_fcs_if.cb.fcs_error);
-      @(i_fcs_if.cb);
-    end
+    @(i_fcs_if.cb); // wait another cycle for fcs_error to be pulsed
 
     // Check result
     if (i_fcs_if.cb.fcs_error == 'b0)begin
-      $display("TEST 1: CRC PASSED");
+      $display("TEST 2 (FIXED PAYLOAD, CORRECT FCS HEADER): PASS");
     end else begin
-      $display("TEST 1: CRC FAILED \n");
-      $display("Payload: 0x%h", payload);
-      $display("CRC: 0x%h", crc);
-      $display("Received frame: 0x%h", frame);
-      $display("fcs_error: b%b", i_fcs_if.cb.fcs_error);
+      $display("TEST 2 (FIXED PAYLOAD, CORRECT FCS HEADER): FAIL \n");
+      $display("got fcs_error: b%b, expected fcs_error = b0", i_fcs_if.cb.fcs_error);
     end
   endtask
 
-  task automatic simple_test_fixed_payload_wrong_crc();
+  task automatic test_fixed_payload_wrong_crc();
       // Fixed payload
     payload = 512'h0010_A47B_EA80_0012_3456_7890_0800_4500_002E_B3FE_0000_8011_0540_C0A8_002C_C0A8_0004_0400_0400_001A_2DE8_0001_0203_0405_0607_0809_0A0B_0C0D_0E0F_1011; // fixed payload for debugging
     crc = 32'hE6C53DB1;
@@ -88,6 +82,38 @@ module tb_fcs_serial_check;
     @(i_fcs_if.cb);
     i_fcs_if.cb.start_of_frame <= 'b0;
 
+    // Send all remaining bits
+    for (int i = fcs.PAYLOAD_LEN + 31 - 1; i >= 0; i--) begin
+      i_fcs_if.cb.data_in      <= frame[i];  // feed bits from MSB to LSB
+      i_fcs_if.cb.end_of_frame <= (i == 31); // first FCS bit
+      @(i_fcs_if.cb);
+      i_fcs_if.cb.end_of_frame <= 'b0;
+    end
+    @(i_fcs_if.cb); // wait one clock cycle for the final crc register update
+
+    @(i_fcs_if.cb); // wait another cycle for fcs_error to be pulsed
+
+    // Check result
+    if (i_fcs_if.cb.fcs_error == 'b1)begin
+      $display("TEST 2 (FIXED PAYLOAD, WRONG FCS HEADER): PASS");
+    end else begin
+      $display("TEST 2 (FIXED PAYLOAD, WRONG FCS HEADER): FAIL");
+      $display("got fcs_error: b%b, expected fcs_error = b1", i_fcs_if.cb.fcs_error);
+    end
+  endtask
+
+  task automatic test_random_payload();
+    // Fixed payload
+    payload = fcs.generate_payload(); // random payload for testing
+    crc = fcs.calc_crc(payload);
+    frame = {payload, crc}; // frame to be fed to fcs_serial_check
+
+    // Send serially
+    i_fcs_if.cb.start_of_frame <= 'b1; // pulse start of frame
+    i_fcs_if.cb.data_in <= frame[fcs.PAYLOAD_LEN + 31]; // first bit of payload with start of frame
+    @(i_fcs_if.cb);
+    i_fcs_if.cb.start_of_frame <= 'b0;
+
     // Remaining bits
     for (int i = fcs.PAYLOAD_LEN + 31 - 1; i >= 0; i--) begin
       i_fcs_if.cb.data_in      <= frame[i];  // feed bits from MSB to LSB
@@ -95,38 +121,60 @@ module tb_fcs_serial_check;
       @(i_fcs_if.cb);
       i_fcs_if.cb.end_of_frame <= 'b0;
     end
+    @(i_fcs_if.cb); // wait one clock cycle for the final crc register update
 
-    // Wait 31 cycles to ensure fcs_serial_check has processed the final bits
-    repeat(31) begin
-      @(i_fcs_if.cb);
-    end
+    @(i_fcs_if.cb); // wait another cycle for fcs_error to be pulsed
 
     // Check result
-    if (i_fcs_if.cb.fcs_error != 'b0)begin
-      $display("TEST 2: CRC PASSED");
+    if (i_fcs_if.cb.fcs_error == 'b0)begin
+      $display("TEST 3 (RANDOM PAYLOAD, CORRECT FCS HEADER): PASS");
     end else begin
-      $display("TEST 2: CRC FAILED \n");
-      $display("Payload: 0x%h", payload);
-      $display("CRC: 0x%h", crc);
-      $display("Received frame: 0x%h", frame);
-      $display("fcs_error: b%b", i_fcs_if.cb.fcs_error);
+      $display("TEST 3 (RANDOM PAYLOAD, CORRECT FCS HEADER): FAIL \n");
+      $display("got fcs_error: b%b, expected fcs_error = b0", i_fcs_if.cb.fcs_error);
     end
   endtask
 
-  // Debugger todo remove
-  task automatic debug_crc_calc();
-    payload = 480'h0010_A47B_EA80_0012_3456_7890_0800_4500_002E_B3FE_0000_8011_0540_C0A8_002C_C0A8_0004_0400_0400_001A_2DE8_0001_0203_0405_0607_0809_0A0B_0C0D_0E0F_1011;
+  task automatic test_random_payload_wrong_crc();
+    // Fixed payload
+    payload = fcs.generate_payload(); // random payload for testing
     crc = fcs.calc_crc(payload);
-    $display("CRC = %h", crc);
-    $finish;
+    crc[31:30] = ~crc[31:30]; // introduce error in the FCS
+    frame = {payload, crc}; // frame to be fed to fcs_serial_check
+
+    // Send serially
+    i_fcs_if.cb.start_of_frame <= 'b1; // pulse start of frame
+    i_fcs_if.cb.data_in <= frame[fcs.PAYLOAD_LEN + 31]; // first bit of payload with start of frame
+    @(i_fcs_if.cb);
+    i_fcs_if.cb.start_of_frame <= 'b0;
+
+    // Remaining bits
+    for (int i = fcs.PAYLOAD_LEN + 31 - 1; i >= 0; i--) begin
+      i_fcs_if.cb.data_in      <= frame[i];  // feed bits from MSB to LSB
+      i_fcs_if.cb.end_of_frame <= (i == 31); // first FCS bit
+      @(i_fcs_if.cb);
+      i_fcs_if.cb.end_of_frame <= 'b0;
+    end
+    @(i_fcs_if.cb); // wait one clock cycle for the final crc register update
+
+    @(i_fcs_if.cb); // wait another cycle for fcs_error to be pulsed
+
+    // Check result
+    if (i_fcs_if.cb.fcs_error == 'b1)begin
+      $display("TEST 4 (RANDOM PAYLOAD, WRONG FCS HEADER): PASS");
+    end else begin
+      $display("TEST 4 (RANDOM PAYLOAD, WRONG FCS HEADER): FAIL \n");
+      $display("got fcs_error: b%b, expected fcs_error = b1", i_fcs_if.cb.fcs_error);
+    end
   endtask
 
+
+  ///////////////////////////// RUN ALL TESTS //////////////////////////////
   initial begin // run tests
     fcs.reset_seq();
-    simple_test_fixed_payload();
-    fcs.reset_seq();
-    simple_test_fixed_payload_wrong_crc();
-    // debug_test();
+    test_fixed_payload();
+    test_fixed_payload_wrong_crc();
+    test_random_payload();
+    test_random_payload_wrong_crc();
     $stop();
   end
 endmodule
